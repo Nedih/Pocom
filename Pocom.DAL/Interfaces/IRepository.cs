33394457bd341f69918f87eb2aeaacc348a0906c
Nedih <49317261+Nedih@users.noreply.github.com>
2022-10-17 13:56:12 +0300

    Added IRepository interface

---
+++ Pocom.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pocom.DAL.Interfaces
+{
+    public interface IRepository
+    {
+        void AddAndSave<TEntity>(TEntity entity) where TEntity : class, IEntity;
+        void UpdateAndSave<TEntity>(TEntity entity) where TEntity : class, IEntity;
+        void RemoveAndSave<TEntity>(TEntity entity) where TEntity : class, IEntity;
+        IList<TEntity> GetWhere<TEntity>(Func<TEntity, bool> predicate) where TEntity : class, IEntity;
+        TEntity FirstorDefault<TEntity>(Func<TEntity, bool> predicate) where TEntity : class, IEntity;
+        int Count<TEntity>(Func<TEntity, bool> predicate) where TEntity : class, IEntity;
+        Task SaveAsync();
+    }
+}
