5b53880f56ec605cad363f46169fd8d9a221f837
Nedih <49317261+Nedih@users.noreply.github.com>
2022-10-17 17:02:58 +0300

    Added repository realization and database context

--- Pocom.DAL/Repositories/Repository.cs
+++ Pocom.DAL/Repositories/Repository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Pocom.DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,45 @@ using System.Threading.Tasks;
 
 namespace Pocom.DAL.Repositories
 {
-    public class Repository
+    public class Repository : IRepository
     {
+        private DbContext context;
 
+        public Repository(PocomContext context)
+        {
+            this.context = context;
+        }
+        
+        TEntity IRepository.FirstorDefault<TEntity>(Func<TEntity, bool> predicate)
+        {
+            return this.context.Set<TEntity>().FirstOrDefault(predicate);
+        }
+        IList<TEntity> IRepository.GetWhere<TEntity>(Func<TEntity, bool> predicate)
+        {
+            return this.context.Set<TEntity>().Where(predicate).ToList();
+        }
+        int IRepository.Count<TEntity>(Func<TEntity, bool> predicate)
+        {
+            return this.context.Set<TEntity>().Count(predicate);
+        }
+        void IRepository.AddAndSave<TEntity>(TEntity entity)
+        {
+            this.context.Set<TEntity>().Add(entity);
+            this.context.SaveChanges();
+        }
+        void IRepository.RemoveAndSave<TEntity>(TEntity entity)
+        {
+            this.context.Set<TEntity>().Remove(entity);
+            this.context.SaveChanges();
+        }
+        void IRepository.UpdateAndSave<TEntity>(TEntity entity)
+        {
+            this.context.Entry(entity).State = EntityState.Modified;
+            this.context.SaveChanges();
+        }
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
     }
 }
