d4d7d407c859d49474d9ba30a10c39ba2633f4cd
Nedih <49317261+Nedih@users.noreply.github.com>
2022-10-17 13:19:38 +0300

    Entities added

---
+++ Pocom.DAL/Entities/Reaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pocom.DAL.Entities
+{
+    public class Reaction
+    {
+        Guid Id { get; set; }
+    }
+}
